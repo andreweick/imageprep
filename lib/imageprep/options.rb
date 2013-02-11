d52ac38f079524c3048fd76153b7680ef3cd3bc5
Andrew Eick <andy@eick.us>
2013-02-10 20:05:46 -0500

    not really sure why it doesn't work

--- lib/imageprep/options.rb
+++ lib/imageprep/options.rb
@@ -1,61 +1,72 @@
 require 'optparse'
 
 module ImagePrep
-  class options
+  class Options
+
+    attr_reader :images_to_load
+    attr_reader :quiet
+    attr_reader :verbose
+    attr_reader :outDir
+    attr_reader :logfile
 
     def initialize(argv)
       parse(argv)
+      @images_to_load = argv
     end
 
     private
-      def parse(argv)
-        options = {}
+    def parse(argv)
 
-        optparse = OptionParser.new do|opts|
-          opts.banner = "Usage: imageprep.rb [options] file1 file2 ..."
+      optparse = OptionParser.new do|opts|
+        opts.banner = "Usage: imageprep.rb [options] file1 file2 ..."
 
-          options[:verbose] = false
-          opts.on('-v', '--verbose', 'Output more information') do
-            options[:verbose] = true
-          end
-
-          options[:quiet] = false
-          opts.on('-q', '--quietly', 'Perform the task quietly') do
-            options[:quiet] = true
-          end
-
-          options[:outDir] = nil
-          opts.on('-o', '--outDir directory', 'Output directory') do|output_directory|
-            options[:outDir] = output_directory
-          end
-
-          options[:logfile] = nil
-          opts.on('-l', '--logfile FILE', 'Write log to FILE') do|file|
-            options[:logfile] = file
-          end
-
-          opts.on( '-h', '--help', 'Display this screen') do
-            puts opts
-            exit
-          end
+        @verbose = false
+        opts.on('-v', '--verbose', 'Output more information') do
+          @verbose = true
         end
 
-        # Parse the command-line. Remember there are two forms
-        # of the parse method. The 'parse' method simply parses
-        # ARGV, while the 'parse!' method parses ARGV and removes
-        # any options found there, as well as any parameters for
-        # the options. What's left is the list of files to resize.
-        optparse.parse!
+        @quiet = false
+        opts.on('-q', '--quietly', 'Perform the task quietly') do
+          @quiet = true
+        end
 
-        mandatory = [:outDir]
-        missing = mandatory.select{ |param| options[param].nil? }
-        if not missing.empty?
-          puts "Missing options: #{missing.join(', ')}"
-          puts optparse
+        @outDir = nil
+        opts.on('-o', '--outDir directory', 'Output directory') do|output_directory|
+          @outDir = output_directory
+        end
+
+        @logfile = nil
+        opts.on('-l', '--logfile FILE', 'Write log to FILE') do|file|
+          @logfile = file
+        end
+
+        opts.on( '-h', '--help', 'Display this screen') do
+          puts opts
           exit
         end
-
       end
+
+      # Parse the command-line. Remember there are two forms
+      # of the parse method. The 'parse' method simply parses
+      # ARGV, while the 'parse!' method parses ARGV and removes
+      # any options found there, as well as any parameters for
+      # the options. What's left is the list of files to resize.
+      begin
+        argv = ["-h"] if argv.empty?
+        opts.parse!(argv)
+      rescue OptionParser::ParseError => e
+        STDERR.puts e.message, "\n", opts
+        exit(-1)
+      end
+
+      # mandatory = [:outDir]
+      # missing = mandatory.select{ |param| options[param].nil? }
+      # if not missing.empty?
+      #   puts "Missing options: #{missing.join(', ')}"
+      #   puts optparse
+      #   exit
+      # end
+
     end
   end
 end
--- test/test_options.rb
+++ test/test_options.rb
@@ -1,16 +1,12 @@
+#!/usr/bin/ruby
+
 require 'rubygems'
 require 'bundler/setup'
 
 require 'test/unit'
-require 'shoulda'
-
-require_relative '../lib/imageprep/options.rb'
+require_relative '../lib/imageprep/options'
 
 class TestOptions < Test::Unit::TestCase
-  context "specifying one file name" do 
-    should "return error" do
-      opts = ImagePrep::Options.new(["file1"])
-      assert_equal
-    end
-  end
+  opts = ImagePrep::Options.new(["-o", "/tmp", "image1", "image2"])
+  assert_equal ["image1", "image2"], opts.images_to_load
 end
