09c1b45bde3adaa86125f721a3cf0e4750407f4a
Andrew Eick <andy@eick.us>
2013-02-16 18:45:27 -0500

    added test_imagesize class

---
+++ test/test_imagesize.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/ruby
+
+require 'rubygems'
+require 'bundler/setup'
+
+require 'test/unit'
+
+require_relative '../lib/imageprep/imagesize'
+
+class TestOptions < Test::Unit::TestCase
+  def test_image
+  end
+end
