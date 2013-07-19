b465138a49c57537eb5e8d08765776af0aed5cb5
Andrew Eick <maeick@missionfocus.com>
2013-07-19 15:34:03 -0400

    not even close to working, but have to start somewhere

--- lib/imageprep/octo.rb
+++ lib/imageprep/octo.rb
@@ -3,16 +3,19 @@
 require 'rubygems'
 require 'bundler/setup'
 
+require_relative 'metadata'
 
 module ImagePrep
   class Octo
-  	attr_reader :image_list, :out_directory 
+  	attr_reader :image_list, :out_directory
+
   	def initialize(image_list, out_directory)
   		@image_list = image_list
   		@out_directory = out_directory
   	end
 
   	def to_octopress
+  		metadata_list = Array.new
   		# Emit the octo-yaml
 			puts "---"
 			puts "layout: post"
@@ -23,31 +26,32 @@ module ImagePrep
 			puts "categories: " 
 			puts "photographs: "
 
-			imgs_yaml.each { |yaml|
-				metadata = YAML.load_file(yaml)
-				puts "- url: http://media.eick.us/#{metadata["path"]}"
-				puts "  headline: \"#{metadata["headline"]}\""
-				puts "  caption: \"#{metadata["caption"]}\""
-				puts "  date_time_original: #{metadata["dateTimeOriginal"]}"
-				puts "  copyright: #{metadata["copyright"]}"
-				puts "  city: #{metadata["city"]}"
-				puts "  state: #{metadata["state"]}"
-				puts "  country: #{metadata["country"]}"
-				puts "  exposure_time: #{metadata["exposureTime"]}"
-				puts "  focal_length: #{metadata["focalLength"]}"
-				puts "  aperture: #{metadata["aperture"]}"
-				puts "  iso: #{metadata["iso"]}"
-				puts "  camera: #{metadata["camera"]}"
-				puts "  markdown: ![#{metadata["headline"]}](http://media.eick.us/images/#{metadata["path"]})"
+			image_list.each { |image|
+				metadata = ImagePrep::MetaData.new(image)
+				metadata_list.push(metadata)				# save the metadata
+
+				puts "- url: http://media.eick.us/#{metadata.path}"
+				puts "  headline: \"#{metadata.headline}\""
+				puts "  caption: \"#{metadata.caption}\""
+				puts "  date_time_original: #{metadata.date_time_original}"
+				puts "  copyright: #{metadata.copyright}"
+				puts "  city: #{metadata.city}"
+				puts "  state: #{metadata.state}"
+				puts "  country: #{metadata.country}"
+				puts "  exposure_time: #{metadata.exposure_time}"
+				puts "  focal_length: #{metadata.focal_length}"
+				puts "  aperture: #{metadata.aperture}"
+				puts "  iso: #{metadata.iso}"
+				puts "  camera: #{metadata.camera}"
+				puts "  markdown: ![#{metadata.headline}](http://media.eick.us/images/#{metadata.path})"
 			}
 			puts "---"
 
 			# and now write out each image in the body 
-			imgs_yaml.each { |yaml|
-				metadata = YAML.load_file(yaml)
-				puts "{% photo2 http://media.eick.us/images/#{metadata["path"]} \"#{metadata["headline"]}\" %}"
+			metadata_list.each { |md|
+				puts "{% photo2 http://media.eick.us/images/#{md.path} \"#{md.headline}\" %}"
 				puts
 			}
-		}
+		end
   end
 end
--- test/test_octo.rb
+++ test/test_octo.rb
@@ -8,44 +8,28 @@ require 'test/unit'
 require 'date'
 require 'fileutils'
 
-require_relative '../lib/imageprep/metadata'
-require_relative '../lib/imageprep/resize'
+require_relative '../lib/imageprep/octo'
 
 class TestOcto < Test::Unit::TestCase
   # Image constant names
-  OctoTestImages = {  
-    landscape:      "./test/data/landscape-big-enough-2895x1930.jpg",
-    portrait:       "./test/data/portrait-big-enough-3840x5760.jpg",
-    notbigenough:   "./test/data/not-big-enough-1333x2000.jpg",
-    needstrip:      "./test/data/2013-01-19 at 10-54-54.jpg",
-    scan:           "./test/data/2013 02 11 20 24 33 jasmine 1.jpg" 
-  }
+  OctoTestImages = [  
+    "./test/data/landscape-big-enough-2895x1930.jpg",
+    "./test/data/portrait-big-enough-3840x5760.jpg",
+    "./test/data/not-big-enough-1333x2000.jpg",
+    "./test/data/2013-01-19 at 10-54-54.jpg",
+    "./test/data/2013 02 11 20 24 33 jasmine 1.jpg" 
+  ]
 
-  def test_imagesExists
-    OctoTestImages.each do |nature, filename|
+  def test_images_exists
+    OctoTestImages.each do |filename|
       assert(File::exists?(filename))
     end
   end
 
   def test_generate_octo
-    image_processed = Array.new
-    Dir.mktmpdir { |dir|
-      OctoTestImages.each do |nature, filename|  
-        rz = ImagePrep::Resize.new(filename, "#{dir}")
-        rz.do_work
-
-        # Write out YAML file describing image
-        md = ImagePrep::MetaData.new(filename).write_yaml(rz.path_original_dir)
-        image_processed.push(ImagePrep::Resize.new(filename, "#{dir}").do_work)
-      end
-
-      image_processed.each { |filename|  
-        assert(File::exists?(filename), "test_generate_octo #{filename} did not get created by Resize")
-        
-        yaml_file = File.join(File.dirname(filename), File.basename(filename, ".*") + ".yaml")
-        assert(File::exists?(yaml_file), "Yaml file #{yaml_file} not created.")
-      }
-    }
+    # Dir.mktmpdir {|dir|
+      ImagePrep::Octo.new(OctoTestImages, "/tmp").to_octopress
+    # }
   end
 
 end
