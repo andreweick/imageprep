184e95c009e42836d18515010c5ca26bcfd857ac
Andrew Eick <maeick@missionfocus.com>
2014-03-03 19:22:06 -0500

    move class to work on a directory of files

--- lib/imageprep/stage.rb
+++ lib/imageprep/stage.rb
@@ -20,14 +20,22 @@ module ImagePrep
     end
 
     def stage_orignal
-      path = File.join(@dest_root, "original", @metadata.root, @metadata.slug_name + @metadata.ext)
+      @path = File.join(@dest_root, "original", @metadata.root, @metadata.slug_name + @metadata.ext)
 
-      FileUtils.mkpath(File.dirname(path))    # create directory path in case doesn't exist
-      FileUtils.cp(@image_file_name, path)
+      FileUtils.mkpath(File.dirname(@path))    # create directory path in case doesn't exist
+      FileUtils.cp(@image_file_name, @path)
       
-      @metadata.write_json(path)
+      @metadata.write_json(@path)
 
-      path
+      @path
+    end
+
+    def write_json
+      json_file_name = "#{File.dirname(@path)}/#{File.basename(@path,'.*')}.json"
+      File.open(json_file_name,'w'){ |file| 
+        file.write(@metadata.to_json) 
+      }
+      json_file_name
     end
   end
 end
